6c56ba28ccb32163f63b67495958c7420c055e1b
deepak <dongdeep27@gmail.com>
2023-02-27 14:24:06 +0530

    My First Commit

---
+++ testGitDemov1/TestGitDemo.java
@@ -0,0 +1,17 @@
+package testGitDemov1;
+
+public class TestGitDemo {
+
+	public static void main(String[] args) {
+		int a=30;
+		int b=10;
+		
+		if(a>b)
+			System.out.println("a is greater" +a);
+		else
+			System.out.println("b is greater" +b);
+				
+	
+	}
+
+}
